c55a24aed468a8651a78c3d1849ab3e18a377661
Phil R <rhinehartphillip@gmail.com>
2022-06-14 21:33:57 -0400

    exercise 4

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { QUERIES, COLORS } from '../../constants';
 
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
@@ -26,14 +27,14 @@ const SpecialtyStoryGrid = () => {
         </MarketCards>
       </MarketsSection>
       <SportsSection>
-        <SectionTitle
+        <SportsTitle
           cornerLink={{
             href: '/sports',
             content: 'Visit Sports page »',
           }}
         >
           Sports
-        </SectionTitle>
+        </SportsTitle>
         <SportsStories>
           {SPORTS_STORIES.map((data) => (
             <MiniStory key={data.id} {...data} />
@@ -47,14 +48,55 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr;
+    gap: 32px;
+  }
 `;
 
 const MarketsSection = styled.section``;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(183px, 1fr));
+  gap: 16px;
 
-const SportsSection = styled.section``;
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    margin-right: -16px;
+  }
+`;
 
-const SportsStories = styled.div``;
+const SportsSection = styled.section`
+
+  @media ${QUERIES.tabletAndUp} {
+    overflow-x: hidden;    
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    border-left: 1px solid ${COLORS.gray[300]};
+    padding-left: 22px;
+  }
+`;
+
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(183px, 1fr));
+  gap: 16px;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+    overflow-x: auto;
+  }
+`;
+
+const SportsTitle = styled(SectionTitle)`
+  @media ${QUERIES.tabletAndUp} {
+    position: sticky;
+    left: 0;
+  }
+`;
 
 export default SpecialtyStoryGrid;
